a4c27118b4112bb294030faff4e8f652126bd47e
Nishant Sevak <160159405+nishantsevak11@users.noreply.github.com>
2025-02-04 20:38:29 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/send', (req,res)=>{
+app.get('/send', async(req,res)=>{
     try{
          await sendDailyEmails();
          res.status(200).json({
